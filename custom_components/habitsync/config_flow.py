c93f0f29d6783b2e32f861a8829c789ec4dc8ff9
jamespdat-spec <jamespdat@gmail.com>
2026-02-05 11:08:36 +0000

    here we go again

--- custom_components/habitsync/config_flow.py
+++ custom_components/habitsync/config_flow.py
@@ -63,8 +63,8 @@ class HabitSyncOptionsFlow(config_entries.OptionsFlow):
             step_id="init",
             data_schema=vol.Schema(
                 {
-                    vol.Optional("sensor_types", default=current_sensors): vol.All(list, [vol.In(list(SENSOR_TYPES.keys()))]),
-                    vol.Optional("features", default=current_features): vol.All(list, [vol.In(list(FEATURES.keys()))]),
+                    vol.Optional("sensor_types", default=current_sensors): vol.All(cv.ensure_list, [vol.In(list(SENSOR_TYPES.keys()))]),
+                    vol.Optional("features", default=current_features): vol.All(cv.ensure_list, [vol.In(list(FEATURES.keys()))]),
                 }
             ),
         )
